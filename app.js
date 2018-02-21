7b948a332c22b459111b447a0240815e2ae3672f
Unknown <llys@palo-it.com>
2018-02-21 17:50:58 +0100

    Add matching engine

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ var express = require('express'),
     fs = require('fs'),
     cron = require('node-cron'),
     bodyParser = require('body-parser'),
-    orderBook = [],
     ask = [],
     bid = [],
-    txnHist = [],
+    txnHistory = [],
     id = 0;
 
 //Setting up json parson
@@ -27,7 +26,7 @@ app.post('/api/order-book', function (req, res) {
 });
 
 app.get('/api/order-book', function(req, res){
-	res.status(200).send(JSON.stringify(orderBook));
+	res.status(200).send(JSON.stringify(ask.concat(bid)));
 });
 
 
@@ -35,7 +34,8 @@ app.get('/api/order-book', function(req, res){
 io.sockets.on('connection', function (socket) {
 	// We update the order book every second
 	cron.schedule('1-59 * * * * *', function(){
-	  socket.broadcast.emit('orderBook',orderBook);
+	  socket.broadcast.emit('orderBook',ask.concat(bid));
+	  socket.broadcast.emit('txnHistory',txnHistory);
 	});
 
 	// When we receive a trade from one of the client, it is added to the order book
@@ -44,26 +44,27 @@ io.sockets.on('connection', function (socket) {
         trade = ent.encode(price, volume, askbid);
        	//Pushing to the order book list
         pushTrade(price,volume,askbid);
+        //Emitting 
+        socket.broadcast.emit('orderBook',ask.concat(bid));
     }); 
+});
+
+cron.schedule('*/10 * * * * *', function(){
+	makeMatchings();
 });
 
 //This function pushes nicely a new trade to a javascript array
 function pushTrade(price, volume, askbid){
 	//We add a unique ID and timestamp 
-	orderBook.push({'id':id,'price':price,'volume':volume,'askbid':askbid, 'time':(new Date()).getTime()});
+	if(askbid === 'ask'){
+		ask.push({'id':id,'price':price,'volume':volume,'askbid':askbid, 'time':(new Date()).getTime()});
+	}
+	if(askbid === 'bid'){
+		bid.push({'id':id,'price':price,'volume':volume,'askbid':askbid, 'time':(new Date()).getTime()});
+	}
 	id++;
 }
 
-//Split order book into two arrays ask and bid
-function splitOrderBook(){
-	orderBook.forEach(function(element) {
-	  if(element['askbid']=='ask'){
-	  	ask.push(element);
-	  }else{
-	  	bid.push(element);
-	  }
-	});
-}
 
 //This functions compares two objects values
 function compare(a,b) {
@@ -84,20 +85,46 @@ return 0;
 
 //Sort arrays in ascending prices
 function sortOrderBook(){
-	splitOrderBook();
 	ask.sort(compare);
 	bid.sort(compare);
-	ask.forEach(function(element){
-		console.log('\nID : ' + element.id +'\nPrice : '+element.price+'\nTime : '+element.time )
-	});
 }
 
 
 function makeMatchings(){
 	sortOrderBook();
+	if (typeof ask[0] !== 'undefined' && ask[0] !== null && typeof bid[bid.length-1] !== 'undefined' && bid[bid.length-1] !== null){
+		try{
+			while(ask[0].price <= bid[bid.length-1].price){
+				txnHistory.push({
+					'price':(ask[0].price + bid[bid.length-1].price)/2,
+					'volume' : Math.min(ask[0].volume, bid[bid.length-1].volume),
+					'askID' : ask[0].id,
+					'bidID' : bid[bid.length-1].id
+				});
+				ask[0].volume = ask[0].volume - Math.min(ask[0].volume, bid[bid.length-1].volume);
+				bid[bid.length-1].volume = bid[bid.length-1].volume - Math.min(ask[0].volume, bid[bid.length-1].volume);
+				if(ask[0].volume == 0){
+					remove(ask, ask[0]);
+					//ask.splice(0,1);
+				}
+				if(bid[bid.length-1] == 0){
+					remove(bid, bid[bid.length-1]);
+					//bid.splice(bid.length-1,1);
+				}
+			}
+		}catch(error){
+			
+		}
+		
+	}
 }
 
-
+function remove(array, element) {
+    const index = array.indexOf(element);
+    if (index !== -1) {
+        array.splice(index, 1);
+    }
+}
 
 
 
